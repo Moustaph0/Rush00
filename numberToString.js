b6fdb2eda39c5e5e375757cb346b41724f345e11
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-12 23:02:09 +0100

    Add files via upload

---
+++ numberToString.js
@@ -0,0 +1,18 @@
+//Declaration d'un tableau
+let tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let tableau;
+// La fontion prend en parametre le tableau et convertie les element du tableau en string
+function numberToString(tableau){
+    tableau[0]="un";
+    tableau[1]="deux";
+    tableau[2]="trois";
+    tableau[3]="quatre";
+    tableau[4]="cinq";
+    tableau[5]="six";
+    tableau[6]="sept";
+    tableau[7]="huit";
+    tableau[8]="neuf";
+    tableau[9]="dix";
+    console.log(tableau);
+}
+numberToString(tab);
